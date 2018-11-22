878cd87ef525a732934af8842055d2039d4f8bff
Richard Tan <chardos@gmail.com>
2018-11-22 23:10:01 +1100

    Update component path to use index.js

--- src/createComponentFile/index.js
+++ src/createComponentFile/index.js
@@ -4,13 +4,12 @@ const makeDir = require('make-dir');
 const { renderSfc } = require('../renderers/react');
 
 module.exports = ({srcPath, componentName}) => {
-  const componentsDirectory = `${srcPath}/components`;
+  const componentsDirectory = `${srcPath}/components/${componentName}`;
 
   makeDir(componentsDirectory).then(() => {
-    const componentPath = `${componentsDirectory}/${componentName}.js`;
+    const componentPath = `${componentsDirectory}/index.js`;
     const actionCode = renderSfc(componentName);
     const prettifiedCode = prettier.format(actionCode, {parser: 'babylon'});
-    console.log('HEY', prettifiedCode)
 
     fs.writeFile(componentPath, prettifiedCode, (err) => {
       if (err) {
