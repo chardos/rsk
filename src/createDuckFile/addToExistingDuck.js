e252f2e2f6c45a919f70de4066a00af617543119
Richard Tan <chardos@gmail.com>
2018-11-26 21:18:24 +1100

    arrow function being inserted

--- src/createDuckFile/addToExistingDuck.js
+++ src/createDuckFile/addToExistingDuck.js
@@ -1,21 +1,30 @@
 const parser = require('@babel/parser').parse;
 const traverse = require('@babel/traverse').default;
 const generate = require('@babel/generator').default;
+const t =  require("@babel/types");
 
 const addToExistingDuck = (reducerName, actions, existingFile) => {
   const ast = parser(existingFile, {sourceType: 'module'});
 
   // TODO: GET THE NEW ACTIONS IN HERE.
-  
+
+  let lastImport;
   traverse(ast, {
-    enter(path) {
-      if (path.node.type === "Identifier") {
-        // console.log('path.node.name', path.node.name);
-        // path.node.name = path.node.name.split("").reverse().join('');
-        // console.log('path.node.name', path.node.name);
-      }
+    ExportNamedDeclaration(path) {
+      lastImport = path;
+      console.log('path.container.unshift',path.container.unshift)
     }
   })
+
+  lastImport.insertAfter(t.arrowFunctionExpression(
+    [],
+    t.blockStatement([
+      t.returnStatement(t.stringLiteral('bro'))
+    ])
+  ));
+
+  // t.importDeclaration( [t.importDefaultSpecifier(t.identifier('tasksReducer'))], t.stringLiteral('./reducers/tasks'), )
+
 
   const newCode = generate(ast).code;
   return newCode
