ede04343b16cf9a0e49cfd5809db4abec1cd7eba
Richard Tan <chardos@gmail.com>
2018-11-28 23:23:40 +1100

    refactor cc and sfc command files

--- src/commands/cc.js
+++ src/commands/cc.js
@@ -1,25 +1,24 @@
 const changeCase = require('change-case');
 const fs = require('fs');
-const prettier = require('prettier');
 const makeDir = require('make-dir');
 const render = require('../renderers/react');
+const { prettify } = require('../utils');
 
-module.exports = (obj) => {
+module.exports = async (obj) => {
   const { srcPath, command, config, positionalArgs } = obj;
   const { componentsDirectory } = config;
 
   const componentName = changeCase.pascalCase(positionalArgs[0]);
   const componentPath = `${srcPath}/${componentsDirectory}/${componentName}`;
 
-  makeDir(componentPath).then(() => {
-    const indexPath = `${componentPath}/index.js`;
-    const actionCode = render[command](componentName);
-    const prettifiedCode = prettier.format(actionCode, { parser: 'babylon' });
+  await makeDir(componentPath);
+  const indexPath = `${componentPath}/index.js`;
+  const actionCode = render[command](componentName);
+  const prettifiedCode = prettify(actionCode);
 
-    fs.writeFile(indexPath, prettifiedCode, (err) => {
-      if (err) {
-        throw new Error(err);
-      }
-    });
+  fs.writeFile(indexPath, prettifiedCode, (err) => {
+    if (err) {
+      throw new Error(err);
+    }
   });
 };
--- src/commands/sfc.js
+++ src/commands/sfc.js
@@ -4,23 +4,21 @@ const { prettify } = require('../utils');
 const makeDir = require('make-dir');
 const render = require('../renderers/react');
 
-module.exports = (obj) => {
+module.exports = async (obj) => {
   const { srcPath, command, config, positionalArgs } = obj;
   const { componentsDirectory } = config;
 
   const componentName = changeCase.pascalCase(positionalArgs[0]);
   const componentPath = `${srcPath}/${componentsDirectory}/${componentName}`;
 
-  makeDir(componentPath).then(() => {
-    // sdf
-    const indexPath = `${componentPath}/index.js`;
-    const actionCode = render[command](componentName);
-    const prettifiedCode = prettify(actionCode);
+  await makeDir(componentPath);
+  const indexPath = `${componentPath}/index.js`;
+  const actionCode = render[command](componentName);
+  const prettifiedCode = prettify(actionCode);
 
-    fs.writeFile(indexPath, prettifiedCode, (err) => {
-      if (err) {
-        throw new Error(err);
-      }
-    });
+  fs.writeFile(indexPath, prettifiedCode, (err) => {
+    if (err) {
+      throw new Error(err);
+    }
   });
 };
