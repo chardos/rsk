8ae134dae8adcb1fc9e06841d59eca603bcc3eba
Richard Tan <chardos@gmail.com>
2018-12-01 09:36:13 +1100

    Refactor parseCommand.js

---
+++ src/commands/index.js
@@ -0,0 +1,11 @@
+// const cc = require('./cc');
+// const reducer = require('./reducer');
+// const sfc = require('./sfc');
+// const setupStore = require('./setup-store');
+
+module.exports = {
+  cc: require('./cc'),
+  reducer: require('./reducer'),
+  sfc: require('./sfc'),
+  'setup-store': require('./setup-store'),
+}
--- src/helpers/parseCommand.js
+++ src/helpers/parseCommand.js
@@ -1,41 +1,8 @@
-const changeCase = require('change-case');
-const createReducerFile = require('../createReducerFile');
-const createActionFile = require('../createActionFile');
-const createDuckFile = require('../createDuckFile');
-const setupStore = require('../commands/setup-store');
-const sfc = require('../commands/sfc');
-const cc = require('../commands/cc');
-const reducer = require('../commands/reducer');
-const { SFC, CC, REDUCER, SETUP_STORE } = require('../constants/commands');
+const commands = require('../commands');
 
 const parseCommand = async (obj) => {
-  const { config, command, positionalArgs, srcPath } = obj;
-  const { style } = config;
-
-  if (command === SFC) {
-    await sfc(obj);
-  }
-
-  if (command === CC) {
-    await cc(obj);
-  }
-
-  if (command === SETUP_STORE) {
-    await setupStore(obj)
-  }
-
-  if (command === REDUCER) {
-    await reducer(obj);
-    // const [reducerName, ...actions] = positionalArgs;
-    // if (style === 'rails') {
-    //   await createReducerFile({ srcPath, reducerName, actions });
-    //   await createActionFile({ srcPath, reducerName, actions });
-    // }
-
-    // if (style === 'ducks') {
-    //   await createDuckFile({ srcPath, reducerName, actions });
-    // }
-  }
+  const { command } = obj;
+  await commands[command](obj);
 };
 
 module.exports = parseCommand;
